a88fb142bf6e7b4071e4bf802c92ac1beea9880c
markgallant01 <markgallant01@gmail.com>
2024-03-30 16:17:32 -0400

    change font to droid

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "DejaVuSansM Nerd Font Mono:pixelsize=20:antialias=true:autohint=true";
+static char *font = "DroidSansM Nerd Font Mono:pixelsize=20:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
